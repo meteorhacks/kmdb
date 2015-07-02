44f8ab4269fb0816c83494702dcf3a89e2c57963
Muhammed Thanish <mnmtanish@gmail.com>
2015-07-02 09:42:52 +0530

    Update tests

--- kmdb/server_test.go
+++ kmdb/server_test.go
@@ -2,6 +2,7 @@ package kmdb
 
 import (
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -22,6 +23,8 @@ var (
 	d kdb.Database
 	s Server
 	c Client
+
+	o DatabaseConfig
 )
 
 // Test Server
@@ -31,7 +34,7 @@ var (
 func init() {
 	os.RemoveAll(DataPath)
 
-	dcfg := DatabaseConfig{
+	dc := DatabaseConfig{
 		DataPath:       DataPath,
 		IndexDepth:     4,
 		PayloadSize:    16,
@@ -44,19 +47,19 @@ func init() {
 		RemoteDebug:   true,
 		ListenAddress: Address,
 		Databases: map[string]DatabaseConfig{
-			Database: dcfg,
+			Database: dc,
 		},
 	}
 
 	dbs := map[string]kdb.Database{}
 	db, err := dbase.New(dbase.Options{
 		DatabaseName:   Database,
-		DataPath:       dcfg.DataPath,
-		IndexDepth:     dcfg.IndexDepth,
-		PayloadSize:    dcfg.PayloadSize,
-		BucketDuration: dcfg.BucketDuration,
-		Resolution:     dcfg.Resolution,
-		SegmentSize:    dcfg.SegmentSize,
+		DataPath:       dc.DataPath,
+		IndexDepth:     dc.IndexDepth,
+		PayloadSize:    dc.PayloadSize,
+		BucketDuration: dc.BucketDuration,
+		Resolution:     dc.Resolution,
+		SegmentSize:    dc.SegmentSize,
 	})
 
 	if err != nil {
@@ -65,6 +68,7 @@ func init() {
 
 	dbs["test"] = db
 	d = db
+	o = dc
 
 	s = NewServer(dbs, cfg)
 	go s.Listen()
@@ -81,14 +85,209 @@ func init() {
 //   Tests
 // ---------
 
-func TestPut(t *testing.T) {
-	// TODO: write test
+func TestEverything(t *testing.T) {
+	ts := time.Now().UnixNano()
+	f1 := []string{"a", "b", "c", "d"}
+	f2 := []string{"a", "b", "c", "D"}
+	g0 := []bool{true, true, true, true}
+
+	// PUT DATA
+	// --------
+
+	b1 := &PutReqBatch{}
+	b1.Batch = make([]*PutReq, 2, 2)
+
+	b1.Batch[0] = &PutReq{
+		Database:  Database,
+		Timestamp: ts - o.Resolution*2,
+		Value:     100.0,
+		Count:     10,
+		Fields:    f1,
+	}
+
+	b1.Batch[1] = &PutReq{
+		Database:  Database,
+		Timestamp: ts - o.Resolution*1,
+		Value:     200.0,
+		Count:     20,
+		Fields:    f2,
+	}
+
+	o1, err := c.Put(b1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// TODO
+	_ = o1
+
+	// GET DATA (get)
+	// --------------
+	// verify put reqs
+
+	b2 := &GetReqBatch{}
+	b2.Batch = make([]*GetReq, 2, 2)
+
+	b2.Batch[0] = &GetReq{
+		Database:  Database,
+		StartTime: ts - o.Resolution*2,
+		EndTime:   ts - o.Resolution*1,
+		Fields:    f1,
+		GroupBy:   g0,
+	}
+
+	b2.Batch[1] = &GetReq{
+		Database:  Database,
+		StartTime: ts - o.Resolution*1,
+		EndTime:   ts,
+		Fields:    f2,
+		GroupBy:   g0,
+	}
+
+	o2, err := c.Get(b2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(o2.Batch) != 2 {
+		t.Fatal("incorrect result count")
+	}
+
+	// verify result no. 1
+	o2r1 := o2.Batch[0]
+
+	if len(o2r1.Data) != 1 {
+		t.Fatal("incorrect series count")
+	}
+
+	o2r1s1 := o2r1.Data[0]
+
+	if !reflect.DeepEqual(o2r1s1.Fields, f1) {
+		t.Fatal("incorrect fields")
+	}
+
+	if len(o2r1s1.Points) != 1 {
+		t.Fatal("incorrect number of points")
+	}
+
+	o2r1s1p1 := o2r1s1.Points[0]
+	if o2r1s1p1.Count != 10 || !feq(o2r1s1p1.Value, 100.0) {
+		t.Fatal("incorrect values")
+	}
+
+	// verify result no. 2
+	o2r2 := o2.Batch[1]
+
+	if len(o2r2.Data) != 1 {
+		t.Fatal("incorrect series count")
+	}
+
+	o2r2s1 := o2r2.Data[0]
+
+	if !reflect.DeepEqual(o2r2s1.Fields, f2) {
+		t.Fatal("incorrect fields")
+	}
+
+	if len(o2r2s1.Points) != 1 {
+		t.Fatal("incorrect number of points")
+	}
+
+	o2r2s1p1 := o2r2s1.Points[0]
+	if o2r2s1p1.Count != 20 || !feq(o2r2s1p1.Value, 200.0) {
+		t.Fatal("incorrect values")
+	}
+
+	// Inc DATA
+	// --------
+
+	b3 := &IncReqBatch{}
+	b3.Batch = make([]*IncReq, 2, 2)
+
+	b3.Batch[0] = &IncReq{
+		Database:  Database,
+		Timestamp: ts - o.Resolution*2,
+		Value:     10.0,
+		Count:     1,
+		Fields:    f1,
+	}
+
+	b3.Batch[1] = &IncReq{
+		Database:  Database,
+		Timestamp: ts - o.Resolution*1,
+		Value:     20.0,
+		Count:     2,
+		Fields:    f2,
+	}
+
+	o3, err := c.Inc(b3)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// TODO
+	_ = o3
+
+	// GET DATA (get)
+	// --------------
+	// verify inc reqs
+
+	// reuse batch b2
+	b4 := b2
+	o4, err := c.Get(b4)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(o4.Batch) != 2 {
+		t.Fatal("incorrect result count")
+	}
+
+	// verify result no. 1
+	o4r1 := o4.Batch[0]
+
+	if len(o4r1.Data) != 1 {
+		t.Fatal("incorrect series count")
+	}
+
+	o4r1s1 := o4r1.Data[0]
+
+	if !reflect.DeepEqual(o4r1s1.Fields, f1) {
+		t.Fatal("incorrect fields")
+	}
+
+	if len(o4r1s1.Points) != 1 {
+		t.Fatal("incorrect number of points")
+	}
+
+	o4r1s1p1 := o4r1s1.Points[0]
+	if o4r1s1p1.Count != 11 || !feq(o4r1s1p1.Value, 110.0) {
+		t.Fatal("incorrect values")
+	}
+
+	// verify result no. 2
+	o4r2 := o4.Batch[1]
+
+	if len(o4r2.Data) != 1 {
+		t.Fatal("incorrect series count")
+	}
+
+	o4r2s1 := o4r2.Data[0]
+
+	if !reflect.DeepEqual(o4r2s1.Fields, f2) {
+		t.Fatal("incorrect fields")
+	}
+
+	if len(o4r2s1.Points) != 1 {
+		t.Fatal("incorrect number of points")
+	}
+
+	o4r2s1p1 := o4r2s1.Points[0]
+	if o4r2s1p1.Count != 22 || !feq(o4r2s1p1.Value, 220.0) {
+		t.Fatal("incorrect values", o4r2s1p1)
+	}
 }
 
-func TestInc(t *testing.T) {
-	// TODO: write test
-}
-
-func TestGet(t *testing.T) {
-	// TODO: write test
+func feq(f1, f2 float64) bool {
+	d := f1 - f2
+	return d < 1.0 && d > -1.0
 }
